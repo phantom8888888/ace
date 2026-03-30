f467de80b299fec9e38ce2f1cc31405c5a860ced
yaokai0928-glitch <yaokai0928@gmail.com>
2026-03-30 16:36:23 +0900

    1

---
+++ lib/11.dart
@@ -0,0 +1,11 @@
+// String name = "hong";
+// int age = 20;
+// double price = 9.99;
+// bool isOk = true;
+
+
+void main(){
+  String lastname="ZHANG";
+  String FIRSTname="CHANG";
+print("name is $lastname $FIRSTname");
+}
---
+++ lib/12.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+void main(){
+int num1 = 10;
+double P1 = 3.14;
+
+double num3=num1*P1;
+num num4 = num1* P1;
+print(num3);
+print(num4);
+
+// String lastname="ZHANG";
+// String FIRSTname="CHANG";
+// print("name is $lastname $FIRSTname");
+
+}
